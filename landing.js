529ff4caf54b8340891b8794558d99bf6adfea56
matigoal <55578343+matigoal@users.noreply.github.com>
2019-09-24 01:27:05 +0200

    Create landing.js

---
+++ landing.js
@@ -0,0 +1,9 @@
+
+  var elem = document.getElementById('suggestion').addEventListener("click", changeColor);;
+ 
+
+function changeColor() {
+  document.getElementById("suggestion").style.background = "#0CB406";
+  document.getElementById("suggestion").innerHTML = "Merci sincèrement";
+
+}
